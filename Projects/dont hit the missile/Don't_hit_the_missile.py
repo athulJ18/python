6e06289ab1e680c33621b747a8da759c4ddce3c5
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-01-12 22:13:19 +0530

    blank window

--- Projects/dont hit the missile/Don't_hit_the_missile.py
+++ Projects/dont hit the missile/Don't_hit_the_missile.py
@@ -1,8 +1,16 @@
-import pygame as pg
+import pygame as pygame
+from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT
 
-from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT,
-
-
-
-# Initialize pygame
+# initializing
 pygame.init()
+window = pygame.display.set_mode((1200, 720))
+pygame.display.set_caption('Dodge the missile')
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_ESCAPE or event.type == QUIT:
+            running = False
+    
+    
